0fd29a810e9eb67e0f32cb6930f1c50bb216063f
MarkZiRo <yhj216@hanmail.net>
2024-11-27 11:41:20 -0800

    update

--- src/main/java/com/jeju/barrierfree/auth/config/WebSecurityConfig.java
+++ src/main/java/com/jeju/barrierfree/auth/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.jeju.barrierfree.auth.config;
 
 import com.jeju.barrierfree.auth.jwt.*;
+import com.jeju.barrierfree.auth.oauth.OAuth2SuccessHandler;
+import com.jeju.barrierfree.auth.oauth.OAuth2UserServiceImpl;
 import com.jeju.barrierfree.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +21,8 @@ public class WebSecurityConfig {
 
     private final JwtTokenUtils jwtTokenUtils;
     private final UserService manager;
-
+    private final OAuth2UserServiceImpl oAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
@@ -36,11 +39,11 @@ public class WebSecurityConfig {
                                 .anyRequest()
                                 .permitAll()
                 )
-//                .oauth2Login(oauth2Login -> oauth2Login
-//                        .loginPage("/login")
-//                        .successHandler(oAuth2SuccessHandler)
-//                        .userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService))
-//                )
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .loginPage("/login")
+                        .successHandler(oAuth2SuccessHandler)
+                        .userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService))
+                )
 //                .exceptionHandling(exceptions -> exceptions
 //                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .sessionManagement(
